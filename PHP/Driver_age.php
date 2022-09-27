4ecfb57398c062f87738dbd6ba0d884adf83e032
Govind yadav <govindsvyadav@gmail.com>
2022-09-27 20:26:02 +0530

    Add files via upload

---
+++ PHP/Driver_age.php
@@ -0,0 +1,15 @@
+<?php
+$age=60;
+echo "Driver age is ".$age;
+echo "<br>";
+$a=25;
+$b=65;
+// And Logic operator
+if($age>=$a && $age<=$b){
+    echo "He can drive";
+}
+else{
+    echo "Not Eligible to drive";
+}
+
+?>
